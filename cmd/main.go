ef50443f2f2ad1bff4c88faac55191d5d8b31d3e
Johan Glud Jensen <johancentralen@gmail.com>
2025-04-26 10:08:32 +0200

    read token file

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	tokenFile, err := os.Open("tmp/tokens.txt")
+	if err != nil {
+		log.Println("could not open tokens file")
+		return
+	}
+
+	var tokens []string
+
+	scanner := bufio.NewScanner(tokenFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+
+		tokens = append(tokens, line)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading from file: ", err)
+	}
+
+	print(tokens)
+}
